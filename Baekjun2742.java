f835fe664001018a22541a77bfca5a439f489e02
Yuntaek Oh <77729974+yuntaekOhO@users.noreply.github.com>
2021-05-23 14:55:02 +0900

    Baekjun2742.java

---
+++ Baekjun2742.java
@@ -0,0 +1,21 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjun2742 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		for(;n>0;n--) bw.write(n+"\n");
+		bw.flush();
+		bw.close();
+	}
+
+}
