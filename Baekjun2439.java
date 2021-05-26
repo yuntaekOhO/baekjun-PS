3353211523476ee593c7d706e79614f9e10fa256
Yuntaek Oh <77729974+yuntaekOhO@users.noreply.github.com>
2021-05-26 17:23:51 +0900

    Baekjun2439.java, Baekjun2440.java

---
+++ Baekjun2439.java
@@ -0,0 +1,28 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjun2439 {
+// x번째 행에 n-x개의 공백, x개의 *
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		for(int i=1;i<=n;i++) {//col
+			for(int j=1;j<=n-i;j++) { //row
+				bw.write(' ');
+			}
+			for(int k=1;k<=i;k++) {
+				bw.write('*');
+			}
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+	}
+}
