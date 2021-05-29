223a0e6e9a50ae3fedad93155299f75c61dad313
Yuntaek Oh <77729974+yuntaekOhO@users.noreply.github.com>
2021-05-29 12:28:56 +0900

    Baekjun10991.java

---
+++ Baekjun10991.java
@@ -0,0 +1,30 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjun10991 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		for(int i=1;i<=n;i++) {
+			for(int j=n-i;j>0;j--) {
+				bw.write(' ');
+			}
+			for(int j=1;j<=i;j++) {
+				bw.write('*');
+				bw.write(' ');
+			}
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
