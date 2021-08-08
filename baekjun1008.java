ba31e559ff7da6c30bb084e75db58fd70fd367b9
Yuntaek Oh <77729974+yuntaekOhO@users.noreply.github.com>
2021-08-08 15:44:46 +0900

    a/b
    변수 a,b를 int 타입으로 정의하면 소숫점을 표현 할 수 없어서 double 타입으로 정의했다.

---
+++ baekjun1008.java
@@ -0,0 +1,21 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjun1008 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		
+		double a = Integer.parseInt(st.nextToken());
+		double b = Integer.parseInt(st.nextToken());
+		System.out.println(a/b);
+	}
+
+}
