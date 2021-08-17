0905666cd5b6ed4b290577b0ef9fca8b8af28c94
Yuntaek Oh <77729974+yuntaekOhO@users.noreply.github.com>
2021-08-17 16:03:57 +0900

    문자열 : 아스키코드
    System.in.read() 로 입력을 읽으면 아스키 코드값이 출력된다.

---
+++ Baekjun11654.java
@@ -0,0 +1,11 @@
+package baekjun;
+
+import java.io.IOException;
+
+public class Baekjun11654 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(System.in.read());
+	}
+
+}
