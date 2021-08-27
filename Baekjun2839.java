54fd22b04cfea41b4427b45c807a820f8939f1ad
Yuntaek Oh <77729974+yuntaekOhO@users.noreply.github.com>
2021-08-27 14:36:13 +0900

    수학1 : 설탕배달
    봉지의 개수를 최소화 하려면 5kg 봉지를 최대한 많이 가져가는게 좋다
    n/5로 몫이 떨어지면 몫이 봉지의 개수이다. 5로 나누어 떨어지는 경우의 n에 +3을 한 n의 봉지의 개수는 +1 이다.
    n이 5의 배수 +1인 경우 봉지의 개수 +1 (3과 6은 5로 채울 수 없으니 6이후 부터 3,6 = 1, 2)
    n이 5의 배수 -1인 경우 9부터 +5 마다 봉지 개수+1 (4와 7은 3, 5kg 으로 채울 수 없다 = -1, n=9부터)
    12의 최소 봉지 수는 4, 12이후 5의 배수의 n 봉지 개수 +1 정리하면
    1. n 이 4 또는 7 인 경우
    2. n 이 5의 배수인경우 ( 5로 나눈 나머지가 0 인 경우 )
    3. n 이 5의 배수 + 1 또는 5의 배수 + 3 인 경우 ( 5로 나눈 나머지가 1 또는 3 인 경우 )
    4. n 이 5의 배수 + 2 또는 5의 배수 + 4 인 경우 ( 5로 나눈 나머지가 2 또는 4 인 경우 )

---
+++ Baekjun2839.java
@@ -0,0 +1,21 @@
+package baekjun;
+import java.io.*;
+public class Baekjun2839 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n==4 || n==7) {
+			System.out.println(-1);
+		} else if(n%5 == 0) {
+			System.out.println(n/5);
+		} else if(n%5==1 || n%5==3) {
+			System.out.println((n/5)+1);
+		} else if(n%5==2 ||  n%5==4) {
+			System.out.println((n/5)+2);
+		}
+	}
+
+}
